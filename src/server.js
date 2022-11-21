7c14026f44386798ecfa83cc3011316a9c5fe3f0
pvtdung0612 <pvtdung0612@gmail.com>
2022-11-21 21:32:36 +0700

    deploy to heroku

--- src/server.js
+++ src/server.js
@@ -2,25 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-import connectDB from "./config/connectDB"
+import connectDB from "./config/connectDB";
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 let app = express();
 
 // config app
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true})) // d34?
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // d34?
 
 viewEngine(app);
 initWebRoutes(app);
 
 connectDB();
 
-let port = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 // PORT === undefined => port = 8080
 
-app.listen(port, () => {
-    console.log("Backend Nodejs is running on the port: " + port)
-})
+app.listen(PORT, () => {
+  console.log("Backend Nodejs is running on the port: " + port);
+});
