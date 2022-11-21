3e6dd73112370c0970472393b65aadb898408835
pvtdung0612 <pvtdung0612@gmail.com>
2022-11-21 21:19:18 +0700

    deploy to heroku

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -24,7 +24,7 @@ const sequelize = new Sequelize(
   validationDB.dbPassword,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "postgres",
     logging: false,
     timezone: process.env.DB_TIMEZONE,
   }
