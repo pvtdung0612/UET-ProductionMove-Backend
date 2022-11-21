dc7183024bc8f21723f84433003893149bdf8e4a
pvtdung0612 <pvtdung0612@gmail.com>
2022-11-21 21:10:40 +0700

    deploy to heroku setup DB with env

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,10 +2,26 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 // Option 3: Passing parameters separately (other dialects)
+let validationDB = {
+  dbName: null,
+  dbUsername: null,
+  dbPassword: null,
+};
+
+if (!(process.env.DB_DATABASE_NAME === "")) {
+  validationDB.dbName = process.env.DB_DATABASE_NAME;
+}
+if (!(process.env.DB_USERNAME === "")) {
+  validationDB.dbUsername = process.env.DB_USERNAME;
+}
+if (!(process.env.DB_PASSWORD === "")) {
+  validationDB.dbPassword = process.env.DB_PASSWORD;
+}
+
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE_NAME,
-  process.env.DB_USERNAME,
-  null,
+  validationDB.dbName,
+  validationDB.dbUsername,
+  validationDB.dbPassword,
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
--- src/models/index.js
+++ src/models/index.js
@@ -6,14 +6,30 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+// const env = process.env.NODE_ENV || "development";
 // const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize = new Sequelize(
-  process.env.DB_DATABASE_NAME,
-  process.env.DB_USERNAME,
-  null,
+let validationDB = {
+  dbName: null,
+  dbUsername: null,
+  dbPassword: null,
+};
+
+if (!(process.env.DB_DATABASE_NAME === "")) {
+  validationDB.dbName = process.env.DB_DATABASE_NAME;
+}
+if (!(process.env.DB_USERNAME === "")) {
+  validationDB.dbUsername = process.env.DB_USERNAME;
+}
+if (!(process.env.DB_PASSWORD === "")) {
+  validationDB.dbPassword = process.env.DB_PASSWORD;
+}
+
+const sequelize = new Sequelize(
+  validationDB.dbName,
+  validationDB.dbUsername,
+  validationDB.dbPassword,
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
